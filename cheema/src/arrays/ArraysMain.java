a943af1ddeb9753d73d3c6b7686d206cf374cf31
BT_1N3_06 <BT_1N3_06@BT_1N3_06.bktech.local>
2017-10-17 15:31:08 -0400

    arrays pt II

--- cheema/src/arrays/ArraysMain.java
+++ cheema/src/arrays/ArraysMain.java
@@ -8,25 +8,107 @@ public class ArraysMain {
 	private String[] values;
 	
 	public ArraysMain() {
-			suits = new String[4];
-			suits[0]= "clubs";
-			suits[1]= "Hearts";
-			suits[2]= "Diamonds";
-			suits[3]= "Spades";
-			values = new String[13];
-			for(int i =0; i < values.length; i++) {
-				values[i] = "" +(i+1);
-			}
-			values[0]="Ace";
-			values[12]="King";
-			values[11]="Queen";
-			values[10]="Jack";
-			printDeck();
+			//warmUpMethods();
+			tuesdayMethods();
 			//populate1toN(testArray);
 			//shuffle(testArray);
 			//populate(testArray);
 			//countOccurances(testArray,2,12);
 			//System.out.println(Arrays.toString(testArray));
+	}
+	private void tuesdayMethods() {
+		int[] orderTest= {1,2,3,4,5,1,6,7,8,9,10,11};
+		//frontToBack(orderTest);
+		//cycleThrough(orderTest,5);
+		//System.out.println(Arrays.toString(orderTest));
+		System.out.println(longestConsecutiveSequence(orderTest) + " is the l.c.s");
+	}
+	private void cycleThrough(int[] arr, int n) {
+		for(int i=0; i <n; i++) {
+			frontToBack(arr);
+		}
+		
+	}
+	/**
+	 * return the length of the longest cons. sequence in the array
+	 * lCS(1,2,3,2,3,4,5,2,3,4)->4
+	 * @param arr
+	 * @return
+	 */
+	public int longestConsecutiveSequence(int[] arr) {
+		int maxLength = 1;
+		int currentCount = 1;
+		
+		for(int i =0; i<arr.length; i++) {
+			while(i+currentCount < arr.length && isConsecutive(arr, i , i+currentCount)) {
+				currentCount++;
+			}
+			if(currentCount > maxLength) {
+				maxLength = currentCount;
+			}
+			i = i+currentCount-1; //saves time
+			currentCount=1;
+		}
+		return maxLength;
+	}
+	private boolean isConsecutive(int[] arr, int start, int end) {
+		for(int i = start; i < end; i++) {
+			if(arr[i]+1 != arr[i+1]) {
+				return false;
+			}
+		}
+		return true;
+	}
+	/**
+	 * removes the element at index 0, pushes all others forward
+	 * 1goes to 0, 2 goes to 1
+	 * puts the element that was at 0 at the end
+	 * @param orderTest
+	 * @return
+	 */
+	private void frontToBack(int[] arr) {
+		int placeholder= arr[0];
+		for(int i =0; i< arr.length-1; i++) {
+			
+			arr[i]=arr[i+1];
+		}
+		arr[arr.length-1]=placeholder;
+	}
+	private void warmUpMethods() {
+		int[] orderTest= {1,2,3,4,5,6,7,8,9,10};
+		reverseOrder(orderTest);
+		System.out.println(Arrays.toString(orderTest));
+		System.out.println(Arrays.toString(subArray(orderTest,3,4)));
+	}
+	public void reverseOrder(int[] arr) {
+		
+		for(int i =0; i < arr.length/2; i++) {
+			swap(arr, i,arr.length-1-i );
+		}
+	}
+	public int[] subArray(int[] arr, int psn, int length) {
+		int[] resultArr = new int[length];
+		for(int i = 0; i <length; i++) {
+			resultArr[i] = arr[i+psn];
+		}
+		return resultArr;
+	}
+	
+	public void cardMethods() {
+		suits = new String[4];
+		suits[0]= "clubs";
+		suits[1]= "Hearts";
+		suits[2]= "Diamonds";
+		suits[3]= "Spades";
+		values = new String[13];
+		for(int i =0; i < values.length; i++) {
+			values[i] = "" +(i+1);
+		}
+		values[0]="Ace";
+		values[12]="King";
+		values[11]="Queen";
+		values[10]="Jack";
+		printDeck();
 	}
 	private String[] printDeck() {
 		String[] deck = new String[52];
